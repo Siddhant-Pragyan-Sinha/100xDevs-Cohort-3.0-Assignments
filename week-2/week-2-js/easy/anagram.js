8300aa4a2a43174b3a1f327887e3658bec578382
Siddhant-Pragyan-Sinha <siddhantpragyansinha16@gmail.com>
2024-12-20 14:23:39 +0530

    h

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -4,8 +4,25 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
+function isAnagram(str1, str2) 
+{
+  const lowercaseString1=str1.toLowerCase();
+  const arr1 = lowercaseString1.split("");
+  arr1.sort();
+  const sortedstring = arr1.join("");
 
+  const lowercaseString2=str2.toLowerCase();
+  const arr2 = lowercaseString2.split("");
+  arr2.sort();
+  const sortedstring2 = arr2.join("");
+  if (sortedstring === sortedstring2) 
+  {
+    return true;
+  } else 
+  {
+    return false;
+  }
 }
-
 module.exports = isAnagram;
+
+
